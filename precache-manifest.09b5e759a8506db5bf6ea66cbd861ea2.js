ecc33824a9011490e2d846a640f4b08a3fe8e49d
Symoom Saad <saadsymoom@gmail.com>
2020-07-07 18:11:57 -0400

    Updates

---
+++ precache-manifest.09b5e759a8506db5bf6ea66cbd861ea2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f9f16f28b191d8eb88b25fdf83e97e",
+    "url": "/HanoiTower/index.html"
+  },
+  {
+    "revision": "9d7603cfff370bf77867",
+    "url": "/HanoiTower/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1d0b5fe2d13eb19966b8",
+    "url": "/HanoiTower/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "9d7603cfff370bf77867",
+    "url": "/HanoiTower/static/js/2.bdcc4b01.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/HanoiTower/static/js/2.bdcc4b01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d0b5fe2d13eb19966b8",
+    "url": "/HanoiTower/static/js/main.5fefa878.chunk.js"
+  },
+  {
+    "revision": "90eb0a31dccf56b81a6a",
+    "url": "/HanoiTower/static/js/runtime-main.693d4528.js"
+  }
+]);
